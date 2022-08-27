dd12c2c86750f5adbea318681ccf4e71c97be49f
Ayaan Mahamed <a-yaaan@hotmail.com>
2022-08-27 17:27:39 +0100

    Syntax homework

---
+++ Syntax.js
@@ -0,0 +1,35 @@
+// i am wring a number here
+console.log (1)
+
+// I am printing a string here
+console.log ("Hello")
+
+//I am creating a variable here and re-declaring it by printing both variables.
+
+let month= "January"
+
+console.log (month)
+
+month=2
+
+console.log (month)
+
+// Making a constant variable
+const number=1
+console.log (number)
+
+//Printing both Boolean types
+console.log (1==1)
+console.log (1===2)
+
+//Create an undefine variable and print it off
+
+var name=
+
+console.log (name)
+
+//creat a null variable and print it off 
+var empty=null
+console.log (empty)
+
+
