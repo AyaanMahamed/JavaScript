614116dd4fd0822831aedf444298fd7c01689c53
Ayaan Mahamed <a-yaaan@hotmail.com>
2022-09-08 20:49:18 +0100

    conditionals homework

---
+++ conditionals.js
@@ -0,0 +1,127 @@
+/*
+var day="Monday"
+if (day == "Monday") {
+  var greeting = "Hello everyone";
+  console.log (greeting)
+  } else {
+    var greeting = "Sorry wrong day";
+    console.log (greeting)
+
+}
+
+{
+const oranges = 2.30;
+const banana = 1.50;
+const mangos = 4.30;
+const apples = 1.20;
+
+
+const budget = 5.50;
+
+var mangosAndApples = apples + mangos
+var mangosAndBanana = mangos + banana 
+var mangosAndOranges = mangos + apples
+
+console.log (mangosAndApples)
+console.log ( mangosAndBanana)
+console.log (mangosAndOranges)
+
+}
+
+
+
+var Mango=4.30
+if (Mango == 4.30) {
+  var greeting = "Hello everyone";
+  console.log (greeting)
+  } else {    
+    var greeting = "Sorry wrong day";
+    console.log (greeting)
+
+}
+
+var number1 = 1;
+var number2 = 1;
+ if (number1 == number2) {
+   console.log ("This number is odd")
+ } else {
+   console.log ("This number is even");
+ }
+
+
+ var gender = "girl"
+ var dob = 1990
+ console.log (name)
+ console.log(name.toLowerCase())
+ console.log(name.toUpperCase())
+ 
+ if (gender === "boy" && dob === 1998){
+ 
+     console.log("Oh this is Abdulkadir")
+ 
+ }else if((gender === "boy") && dob === 1997){
+ 
+     console.log("Look its Feisal")
+ 
+ }else if((gender != "boy") && dob === 2007){
+ 
+     console.log("How can I forget Yasmin")
+ 
+ }else{
+ 
+     console.log("Its Qaasim and Sacdiya")
+ 
+ }
+*/
+
+ var name = "Ridwan"
+ var gender = "boy"
+ var surname = "Qaasim"
+ var mum = "amina"
+ console.log (name)
+ console.log(name.toLowerCase())
+ console.log(name.toUpperCase())
+ 
+ if (name === "Ayaan" && gender === "girl" && (surname === "Qaasim" || mum === "Sacdiya")){
+ 
+     console.log(`Oh this is ${name}`)
+ 
+ }else if((name === "Yasmin" && gender === "girl" && (surname === "Qaasim" || mum === "Sacdiya"))){
+ 
+     console.log (`This is ${name}`)
+ 
+ }else if((name === "Feisal" && gender === "boy" && (surname === "Qaasim" || mum === "Sacdiya"))){
+ 
+     console.log(`This is ${name}`)
+
+ }else if((name === "Abdulkadir" && gender === "boy" && (surname === "Qaasim" || mum === "Sacdiya"))){
+ 
+      console.log(`This is ${name}`)
+
+ }else if(name === "Ridwan" && gender === "boy" && (surname === "Qaasim" && mum !== "Sacdiya")){
+      console.log(`This is ${name}`)
+
+ }else if((name === "Rihanna" && gender === "girl"&& (surname === "Qaasim" && mum != "Sacdiya"))){
+ 
+      console.log(`This is ${name}`)
+
+ }else if((name === "Marwa" && gender === "girl"&& (surname === "Qaasim" && mum != "Sacdiya"))){
+ 
+      console.log(`This is ${name}`)
+
+ }else if((name === "Yasir" && gender === "boy"&& (surname === "Qaasim" && mum != "Sacdiya"))){
+ 
+      console.log(`This is ${name}`)
+
+ }else if((name === "Ambar" && gender === "girl"&& (surname === "Qaasim" && mum != "Sacdiya"))){
+ 
+      console.log(`This is ${name}`)
+    
+   
+ }else{
+ 
+     console.log("This must be an alien")
+ 
+ }
+
+ 
